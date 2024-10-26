b5b4ac9e9c85322f9e52a377e71506420a48085f
gotikaaa <denisbondarenko1231@gmail.com>
2024-10-26 14:43:52 +0300

    Create lab1.sh

---
+++ lab1.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+until rm -rf `find lab0 2> /dev/null` 2> /dev/null; do
+chmod -R 777 lab0 2> /dev/null
+done
+mkdir lab0
+cd lab0
+touch eevee4
+touch exploud7
+mkdir ninjask4
+cd ninjask4
+mkdir milotic
+mkdir mareep
+touch elekid
+touch blitzle
+mkdir crawdaunt
+touch ferroseed
+cd ..
+mkdir roggenrola2
+cd roggenrola2
+mkdir serperior
+mkdir chikorita
+mkdir scraggy
+touch gligar
+cd ..
+touch sealeo5
+mkdir whimsicott4
+cd whimsicott4
+mkdir vanillish
+mkdir poliwrath
+touch mudkip
+touch hariyama
+cd ..
+echo "Ходы Body Slam Covet Double-Edge Heal Bell Helping Hand Hyper
+Voice Iron Tail Last Resort Mud-Slap Sleep Talk Snore
+Swift">eevee4
+echo "Развитые способности Scrappy">exploud7
+cd ninjask4	
+echo "Живет Forest
+Grassland Urban">elekid
+echo "Тип диеты Herbivore">blitzle
+echo "Способности
+Overgrow Unbreakable Iron Barbs Battle Armor">ferroseed
+cd ..
+echo "Способности Poison Sting Sand-Attack Harden Knock Off Quick Attack Fury Cutter
+Faint Attack Acrobatics Slash U-Turn Screech X-Scissor Sky Uppercut
+Swords Dance Guillotine">roggenrola2/gligar	
+echo "Возможности Overland=6 Surface=9
+Underwater=7 Jump=3 Power=3 Intelligence=4 Icestep=0">sealeo5
+echo "Тип диеты
+Herbivore">whimsicott4/mudkip
+echo "Способности Sand-Attack Arm Thrust Vital Throw
+Fake Out Whirlwind Knock Off Smellingsalt Belly Drum Force Palm
+Seismic Toss Wake-Up Slap Endure Close Combat Reversal Heavy m">whimsicott4/hariyama
+chmod 004 eevee4
+chmod 006 exploud7
+chmod 363 ninjask4
+chmod 311 ninjask4/milotic
+chmod 550 ninjask4/mareep
+chmod 004 ninjask4/elekid
+chmod 404 ninjask4/blitzle
+chmod 307 ninjask4/crawdaunt
+chmod 444 ninjask4/ferroseed
+chmod 361 roggenrola2
+chmod 511 roggenrola2/serperior
+chmod 750 roggenrola2/chikorita
+chmod 753 roggenrola2/scraggy
+chmod 440 roggenrola2/gligar
+chmod 066 sealeo5
+chmod 550 whimsicott4
+chmod 555 whimsicott4/vanillish
+chmod 612 whimsicott4/poliwrath
+chmod 512 whimsicott4/poliwrath
+chmod 622 whimsicott4/mudkip
+chmod 440 whimsicott4/hariyama
+echo "3.1"
+ln -s ninjask4 Copy_14
+echo "3.2"
+ln -s exploud7 ninjask4/elekidexploud
+echo "3.3"
+cat roggenrola2/gligar whimsicott4/mudkip > eevee4_33
+echo "3.4"
+ln exploud7 ninjask4/ferroseedexploud
+chmod 777 roggenrola2
+chmod 777 roggenrola2/scraggy
+chmod 777 exploud7
+echo "3.5"
+cp exploud7 roggenrola2/scraggy
+chmod 763 roggenrola2/scraggy
+chmod 006 exploud7
+chmod 777 whimsicott4
+chmod 777 whimsicott4/poliwrath
+echo "3.6"
+cp -r roggenrola2 whimsicott4/poliwrath
+chmod 361 roggenrola2
+chmod 550 whimsicott4
+chmod 512 whimsicott4/poliwrath
+chmod 777 eevee4
+echo "3.7"
+cp eevee4 ninjask4/ferroseedeevee
+chmod 004 eevee4
+echo "Иерархия файлов"
+ls -lR
+echo "4.1"
+wc -c ninjask4/elekid ninjask4/blitzle ninjask4/ferroseed roggenrola2/gligar whimsicott4/mudkip 2>&1 | sort -n
+echo "4.2"
+ls roggenrola2 2>/dev/null| sort
+echo "4.3"
+[ -d lab0 ] && cat -n $(ls -R lab0 | grep 'p$' | sort -r)
+echo "4.4"
+cat -n ninjask4/elekid ninjask4/blitzle ninjask4/ferroseed roggenrola2/gligar whimsicott4/mudkip 2>&1 | grep -v 'm$'
+cd /tmp
+touch lab0123
+cd 
+cd lab0
+echo "4.5"
+cat ninjask4/elekid ninjask4/blitzle ninjask4/ferroseed | grep -i 'vo' 2> /tmp/lab0123
+echo "4.6"
+ls -lR .  2>&1 | grep '^e' | sort -k 2 | tail -n 2
+echo "5.1"
+chmod 777 sealeo5
+rm sealeo5
+echo "5.2"
+chmod 777 roggenrola2/gligar
+rm roggenrola2/gligar
+echo "5.3"
+ls -l Copy_* | grep '^l' | awk '{print $9}' | rm -f
+echo "5.4"
+rm -rf lab0/ninjask4/ferroseedexplo*
+echo "5.5"
+chmod -R 777 ninjask4
+rm -rf ninjask4
+echo "5.6"
+chmod -R 777 whimsicott4
+rm -rf whimsicott4/poliwrath
+chmod 550 whimsicott4
